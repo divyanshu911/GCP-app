fa22bfc4e65bd150419ee3e0e15b54f5021ccd72
TheExperimentalDweebist <abraham.roshan23@gamil.com>
2020-09-08 01:43:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'fuck  you bitch'
+    return 'ExperimentalDweebist  - hello  world'
 
 @app.errorhandler(500)
 def server_error(e):
